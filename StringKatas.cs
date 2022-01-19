3970da8aa2ea98e87a7c2e52e0a13e98c7d1c31a
slawkovvl <slawkovvl@gmail.com>
2022-01-19 16:44:44 +0100

    old pc 19.01.2022

--- StringKatas.cs
+++ StringKatas.cs
@@ -21,9 +21,29 @@ namespace CodeWarsKatas
         {
             string[] names = str.Split(" ");
 
-            return names[1] + " " + names[0];        }
+            return names[1] + " " + names[0];   
+        }
 
-        public static bool Feast(string beast, string dish) => ((beast.Substring(0, 1) == dish.Substring(0, 1)) && (beast.Substring(beast.Length - 1, 1) == (dish.Substring(dish.Length - 1, 1))));      
+        public static bool Feast(string beast, string dish) => ((beast.Substring(0, 1) == dish.Substring(0, 1)) && (beast.Substring(beast.Length - 1, 1) == (dish.Substring(dish.Length - 1, 1))));
+
+        public static string ReverseWords(string str) // toDo not working as expected !!!!
+        {
+            string[] worlds = str.Split(" ");
+            string[] responses = new string[worlds.Length];
+            for (int i = 0; i < worlds.Length; i++)
+            {
+                Array.Reverse(worlds[i].ToCharArray());
+                responses[i] = new string(worlds[i]);
+            }
+
+            string response="";
+            for(int i = 0; i < responses.Length; i++)
+            {
+                response += responses[i];
+            }
+
+            return response;
+        }
 
     }
 }
